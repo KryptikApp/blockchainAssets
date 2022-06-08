17c6348d36bf774c487012de5e0883beac7ae635
jettblu <jettblu.fabrications@gmail.com>
2022-06-08 14:39:34 -0700

    Add upload wrapper function

--- runUploads.ts
+++ runUploads.ts
@@ -1,6 +1,16 @@
 // a file that batches all uploads 
-import { uploadErc20Data } from "./helpers/uploadToFirebase";
+import { uploadErc20Data, uploadNetworkData } from "./helpers/uploadToFirebase";
 
-uploadErc20Data().then(()=>{
+const uploadAll = async function(){
+    await uploadErc20Data();
     console.log("FINISHED UPLOADING ERC20 DATA");
+    console.log("   ---------   ")
+
+    await uploadNetworkData();
+    console.log("FINISHED UPLOADING NETWORK DATA");
+    console.log("   ---------   ")
+}
+
+uploadAll().then(()=>{
+    console.log("UPLOAD COMPLETE!")
 });
