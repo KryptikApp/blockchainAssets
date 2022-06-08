5b812043c584a7e63dd4075d7b1a707895b01426
jettblu <jettblu.fabrications@gmail.com>
2022-06-08 14:41:20 -0700

    Add network models

--- models/index.ts
+++ models/index.ts
@@ -17,3 +17,30 @@ export interface ERC20Data{
     },
     tags:string[]
 }
+
+export interface NetworkDb{
+    fullName: string,
+    ticker: string,
+    iconPath: string,
+    isSupported: boolean,
+    about: string,
+    whitePaperPath: string,
+    chainId: number,
+    chainIdEVM: number,
+    hexColor: string,
+    dateCreated: Date,
+    provider:string,
+    networkFamilyName:string,
+    coingeckoId:string
+    isTestnet: boolean
+}
+
+export interface NetworkSecretParameters{
+    provider: string
+}
+export class NetworkSecretData{
+    readonly provider: string
+    constructor(secretParams:NetworkSecretParameters) {
+        this.provider = secretParams.provider
+    }
+}
