f8452d617e00d3795abcf6306f198a3bd8dbe84b
jettblu <jettblu.fabrications@gmail.com>
2022-06-10 23:01:20 -0700

    Update network model to include explorer url

--- models/index.ts
+++ models/index.ts
@@ -33,6 +33,7 @@ export interface NetworkDb{
     networkFamilyName:string,
     coingeckoId:string
     isTestnet: boolean
+    blockExplorerURL:string
 }
 
 export interface NetworkSecretParameters{
