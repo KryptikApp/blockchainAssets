605479b5baec95fad5a074456d5dc5405b30f514
jettblu <jettblu.fabrications@gmail.com>
2022-06-14 14:34:37 -0700

    Add upload spl data method

--- helpers/uploadToFirebase.ts
+++ helpers/uploadToFirebase.ts
@@ -1,18 +1,29 @@
 import { setDoc } from "firebase/firestore";
 import erc20Json from "../data/erc20/erc20.json"
+import splJson from "../data/spl/spl.json"
 import networkJson from "../data/networks/networks.json"
-import { ERC20Data, NetworkDb } from "../models";
-import { generateDocErc20, generateDocNetwork } from "./firebaseHelper";
+import { NetworkDb, TokenData} from "../models";
+import { generateDocErc20, generateDocNetwork, generateDocSpl } from "./firebaseHelper";
 import { addNetworkSecretData } from "./utils";
 
 
 export const uploadErc20Data = async function(){
-    let erc20DataArray:{erc20Tokens: ERC20Data[]} = JSON.parse(JSON.stringify(erc20Json));
+    let erc20DataArray:{erc20Tokens: TokenData[]} = JSON.parse(JSON.stringify(erc20Json));
     for(const ercData of erc20DataArray.erc20Tokens){
         console.log(`Uploading erc20 data for ${ercData.name}...`);
         let docToWrite = generateDocErc20(ercData);
         await setDoc(docToWrite, ercData);
         console.log(`${ercData.symbol} upload complete`);
+    }
+}
+
+export const uploadSplData = async function(){
+    let splDataArray:{splAssets: TokenData[]} = JSON.parse(JSON.stringify(splJson));
+    for(const splData of splDataArray.splAssets){
+        console.log(`Uploading spl data for ${splData.name}...`);
+        let docToWrite = generateDocSpl(splData);
+        await setDoc(docToWrite, splData);
+        console.log(`${splData.symbol} upload complete`);
     }
 }
 
