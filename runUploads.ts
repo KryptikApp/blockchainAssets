ca90a3f0e0b2b6d0914ffd17f942d2a9f4a630de
jettblu <jettblu.fabrications@gmail.com>
2022-06-14 17:45:32 -0700

    Add spl data upload to run routine

--- runUploads.ts
+++ runUploads.ts
@@ -1,9 +1,13 @@
 // a file that batches all uploads 
-import { uploadErc20Data, uploadNetworkData } from "./helpers/uploadToFirebase";
+import { uploadErc20Data, uploadNetworkData, uploadSplData } from "./helpers/uploadToFirebase";
 
 const uploadAll = async function(){
     await uploadErc20Data();
     console.log("FINISHED UPLOADING ERC20 DATA");
+    console.log("   ---------   ")
+
+    await uploadSplData();
+    console.log("FINISHED UPLOADING SPL DATA");
     console.log("   ---------   ")
 
     await uploadNetworkData();
