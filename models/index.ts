32aa360f06f58405f079b73def08d85f4b5d3a93
Jett Hays <jettblu.fabrications@gmail.com>
2022-12-22 16:25:03 -0800

    Update network model

--- models/index.ts
+++ models/index.ts
@@ -1,54 +1,56 @@
-export interface ChainData{
-    chainId: number,
-    ticker:string,
-    address: string
+export interface ChainData {
+  chainId: number;
+  ticker: string;
+  address: string;
 }
 
-export interface TokenData{
-    name: string,
-    symbol: string,
-    coingeckoId: string,
-    decimals: number,
-    hexColor: string,
-    chainData: ChainData[],
-    logoURI: string,
-    extensions: {
-       link: string,
-       description: string
-    },
-    tags:string[]
+export interface TokenData {
+  name: string;
+  symbol: string;
+  coingeckoId: string;
+  decimals: number;
+  hexColor: string;
+  chainData: ChainData[];
+  logoURI: string;
+  extensions: {
+    link: string;
+    description: string;
+  };
+  tags: string[];
 }
 
-export interface EVMData{
-    chainId: number,
-    zeroXSwapUrl?: string
+export interface EVMData {
+  chainId: number;
+  zeroXSwapUrl?: string;
 }
 
-export interface NetworkDb{
-    fullName: string,
-    ticker: string,
-    iconPath: string,
-    isSupported: boolean,
-    about: string,
-    whitePaperPath: string,
-    chainId: number,
-    evmData?: EVMData,
-    decimals: number,
-    hexColor: string,
-    dateCreated: Date,
-    provider:string,
-    networkFamilyName:string,
-    coingeckoId:string
-    isTestnet: boolean
-    blockExplorerURL:string
+export interface NetworkDb {
+  fullName: string;
+  ticker: string;
+  iconPath: string;
+  isSupported: boolean;
+  about: string;
+  whitePaperPath: string;
+  chainId: number;
+  evmData?: EVMData;
+  decimals: number;
+  hexColor: string;
+  dateCreated: Date;
+  provider: string;
+  networkFamilyName: string;
+  coingeckoId: string;
+  isTestnet: boolean;
+  blockExplorerURL: string;
+  // chain agnostic id
+  blockchainId: string;
 }
 
-export interface NetworkSecretParameters{
-    provider: string
+export interface NetworkSecretParameters {
+  provider: string;
 }
-export class NetworkSecretData{
-    readonly provider: string
-    constructor(secretParams:NetworkSecretParameters) {
-        this.provider = secretParams.provider
-    }
+export class NetworkSecretData {
+  readonly provider: string;
+  constructor(secretParams: NetworkSecretParameters) {
+    this.provider = secretParams.provider;
+  }
 }
