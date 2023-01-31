19800beb9d32ad3ebe7639ec75275b57d8869db3
jettblu <jettblu.fabrications@gmail.com>
2023-01-31 12:59:04 -0500

    Add formatting and contract helpers

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,5 +1,10 @@
-import { NetworkDb } from "@prisma/client";
-import { NetworkSecretData } from "../models";
+import { NetworkDb, TokenDb } from "@prisma/client";
+import {
+  ChainData,
+  NetworkSecretData,
+  TokenData,
+  TokenUpload,
+} from "../models";
 import { NetworkSecretsDict } from "../secrets";
 
 export const addNetworkSecretData = function (network: NetworkDb) {
@@ -8,3 +13,38 @@ export const addNetworkSecretData = function (network: NetworkDb) {
     network.provider = nwSecretData.provider;
   }
 };
+
+/**Returns list of all chain data referenced by tokens.*/
+export function getAllChainData(tokens: TokenData[]): ChainData[] {
+  const result: ChainData[] = tokens.map((t) => t.chainData).flat();
+  return result;
+}
+
+/**Turns list of networks into dictionary. Each entry has a ticker as key and a network as a value. */
+export function buildNetworkDict(networks: NetworkDb[]) {
+  const networkDict: { [ticker: string]: NetworkDb } = {};
+  networks.map((n) => {
+    networkDict[n.ticker] = n;
+  });
+  return networkDict;
+}
+
+export function formatTokenDataForUpload(token: TokenData): TokenUpload {
+  const newToken: TokenUpload = {
+    coingeckoId: token.coingeckoId,
+    description: token.description,
+    decimals: token.decimals,
+    hexColor: token.hexColor,
+    link: token.link,
+    logoURI: token.logoURI,
+    name: token.name,
+    tags: token.tags,
+    ticker: token.ticker,
+  };
+  return newToken;
+}
+
+/** Returns string of the format : "networkTicker:tokenTicker" */
+export function createContractId(token: TokenDb, network: NetworkDb) {
+  return `${network.ticker}:${token.ticker}`;
+}
