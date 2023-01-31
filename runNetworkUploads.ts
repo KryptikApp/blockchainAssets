cad30f4021cb8ddaaaeb508a2d07b6a2f05bac8e
jettblu <jettblu.fabrications@gmail.com>
2023-01-31 00:43:50 -0500

    Update network upload routine

--- runNetworkUploads.ts
+++ runNetworkUploads.ts
@@ -1,52 +1,19 @@
-// a file that batches all uploads 
+// a file that batches all uploads
 import { onAuthStateChanged } from "firebase/auth";
 import { loginToAccount } from "./helpers/authentication";
 import { firebaseAuth } from "./helpers/firebaseHelper";
-import { uploadNetworkData} from "./helpers/uploadToFirebase";
-import { authEmail, authPassword } from "./secrets";
+import { uploadNetworkData } from "./helpers/uploadToDb";
 
-const uploadNetworks = async function():Promise<boolean>{
-    try{
-        await uploadNetworkData();
-        console.log("FINISHED UPLOADING NETWORK DATA");
-        console.log("   ---------   ")
-        return true;
-    }
-    catch(e){
-        return false;
-    }
-}
+const uploadNetworks = async function (): Promise<boolean> {
+  try {
+    await uploadNetworkData();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
 
-onAuthStateChanged(firebaseAuth, authStateChanged);
-
-// login and upload!!
-loginToAccount(firebaseAuth, authEmail, authPassword).then((loginSuccessful:boolean)=>{
-    if(!loginSuccessful){
-        console.log("Error authenticating. Exiting upload process.")
-        return;
-    }
-    else{
-        console.log("Running upload process via callback...");
-    }
-})
-
-
-function authStateChanged(user:any){
-    if(user==null) return;
-    console.log("current user id:");
-    console.log(firebaseAuth.currentUser.uid);
-    console.log("-------");
-    try{
-        uploadNetworks().then((uploadSuccessful:boolean)=>{
-            if(uploadSuccessful){
-                console.log("UPLOAD COMPLETE!")
-            }   
-            else{
-                return;
-            }
-        });
-    }
-    catch(e){
-        console.log("Unable to upload docs")
-    }
-}
+uploadNetworks().then(() => {
+  console.log("FINISHED UPLOADING NETWORK DATA");
+  console.log("   ---------   ");
+});
